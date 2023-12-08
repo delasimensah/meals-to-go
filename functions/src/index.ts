39e3d5c509285889a2c98cffc8d0e95a60586dda
delasimensah <mensadelasi@gmail.com>
2023-12-08 12:13:25 +0000

    setup environment variables

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,7 @@
 import * as logger from "firebase-functions/logger";
 import { onRequest } from "firebase-functions/v2/https";
 
-// google api key - AIzaSyDPbR4xaNNo16KVu_C6Gh5RDx5UaxUdEEs
 export const helloWorld = onRequest((_, response) => {
   logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  response.send(`Hello from Firebase! ${process.env.GOOGLE_API_KEY}`);
 });
