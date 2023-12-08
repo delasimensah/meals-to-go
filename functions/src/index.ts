943aeeb72cd3dc8e45bfe781cfa76aa105ff3d4a
delasimensah <mensadelasi@gmail.com>
2023-12-08 11:58:54 +0000

    installed packages

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import { onRequest } from "firebase-functions/v2/https";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// google api key - AIzaSyDPbR4xaNNo16KVu_C6Gh5RDx5UaxUdEEs
+export const helloWorld = onRequest((_, response) => {
+  logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
