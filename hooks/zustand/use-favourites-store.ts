d2a81aeb825fc24a2df3139f6fb9764e08f35cbd
delasimensah <mensadelasi@gmail.com>
2023-12-25 14:50:10 +0000

    finished favourites feature

--- hooks/zustand/use-favourites-store.ts
+++ hooks/zustand/use-favourites-store.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 import { Restaurant } from "@/types";
 
@@ -7,7 +9,12 @@ type FavouritesStore = {
   setFavourites: (favourites: Restaurant[]) => void;
 };
 
-export const useFavouritesStore = create<FavouritesStore>((set) => ({
-  favourites: [],
-  setFavourites: (favourites) => set({ favourites }),
-}));
+export const useFavouritesStore = create<FavouritesStore>()(
+  persist(
+    (set) => ({
+      favourites: [],
+      setFavourites: (favourites) => set({ favourites }),
+    }),
+    { name: "favourites", storage: createJSONStorage(() => AsyncStorage) },
+  ),
+);
